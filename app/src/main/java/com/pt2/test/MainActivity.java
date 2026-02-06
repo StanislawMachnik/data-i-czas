8ccd8a30ce64991563d69c861a35969a5cdb1285
StanislawMachnik <stanislaw.machnik@uczen.mechaniktg.pl>
2026-02-06 10:18:46 +0100

    Przyciski + dane

--- app/src/main/java/com/pt2/test/MainActivity.java
+++ app/src/main/java/com/pt2/test/MainActivity.java
@@ -1,9 +1,13 @@
 package com.pt2.test;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +15,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     private DatePicker data_picker;
     private TimePicker czas_picker;
+    private Button button;
+    private  Button button2;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +32,36 @@ public class MainActivity extends AppCompatActivity {
 
         data_picker = findViewById(R.id.data_picker);
         czas_picker = findViewById(R.id.czas_picker);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int godzina = czas_picker.getHour();
+                        int minuta = czas_picker.getMinute();
+                        int rok = data_picker.getYear();
+
+                        Toast.makeText(MainActivity.this, "Godzina: " + godzina + " minuta: " + minuta + " rok: " + rok, Toast.LENGTH_SHORT).show();
+
+                        Date date = new Date();
+                        Toast.makeText(MainActivity.this, ""+date, Toast.LENGTH_SHORT).show();
+
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(Calendar.YEAR, data_picker.getYear());
+                        calendar.set(Calendar.MINUTE, czas_picker.getMinute());
+                        Toast.makeText(MainActivity.this, "" + calendar.get(Calendar.YEAR) + " " + calendar.get(Calendar.MINUTE), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+        button2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        DatePickerDialog datePickerDialog = new DatePickerDialog();
+                    }
+                }
+        );
     }
 }
